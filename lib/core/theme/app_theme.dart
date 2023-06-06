81b475fd7c960138f37539e36130e38fbc284194
Ibrahim Ibrahim <39105147+devwraithe@users.noreply.github.com>
2023-06-06 09:35:41 +0100

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -117,6 +117,74 @@ class AppTextTheme {
   );
 }
 
+// default
+// @immutable
+// class AppTextTheme {
+//   static const textTheme = TextTheme(
+//     displayLarge: TextStyle(
+//       fontSize: 56,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w700,
+//     ),
+//     displayMedium: TextStyle(
+//       fontSize: 40,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w700,
+//     ),
+//     displaySmall: TextStyle(
+//       fontSize: 28,
+//       fontWeight: FontWeight.w400,
+//       color: AppColors.black,
+//     ),
+//     headlineLarge: TextStyle(
+//       fontSize: 26,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w700,
+//     ),
+//     headlineMedium: TextStyle(
+//       fontSize: 20,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w700,
+//     ),
+//     headlineSmall: TextStyle(
+//       fontSize: 18,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w400,
+//     ),
+//     // subtitles
+//     titleLarge: TextStyle(
+//       fontSize: 16,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w700,
+//     ),
+//     titleMedium: TextStyle(
+//       fontSize: 14,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w700,
+//     ),
+//     bodyLarge: TextStyle(
+//       fontSize: 16,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w400,
+//     ),
+//     bodyMedium: TextStyle(
+//       fontSize: 14,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w400,
+//     ),
+//     labelLarge: TextStyle(
+//       fontSize: 13,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w400,
+//     ),
+//     labelMedium: TextStyle(
+//       fontSize: 12,
+//       color: AppColors.black,
+//       fontWeight: FontWeight.w700,
+//     ),
+//   );
+// }
+
 class AppColors {
   static const Color white = Color(0XFFFFFFFF);
   static const Color black = Color(0XFF212121);
